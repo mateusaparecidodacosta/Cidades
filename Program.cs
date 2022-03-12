f694c92574cded931bffe1237dcba821e88014b9
Mateus <mateusadc19@gmail.com>
2022-03-11 22:25:16 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Praia Grande");
+Console.WriteLine("Guarujá");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Santos");
+Console.WriteLine("Itanhaém");
